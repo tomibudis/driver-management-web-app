48d90cbb3e80d6fbbb8b3715fb2c17f1ed09c0d7
tomibudis <tomibudis@gmail.com>
2022-05-08 13:07:46 +0700

    chore: component preparation

---
+++ src/components/SearchControl/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SearchControl = () => <div>SearchControl</div>;
+
+export default SearchControl;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
-export * from ".";
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Header } from "./Header";
+export { default as Pagination } from "./Pagination";
+export { default as SearchControl } from "./SearchControl";
