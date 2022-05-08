5784b3b2b6bdb02d5e2a6343d907eb041a51910d
tomibudis <tomibudis@gmail.com>
2022-05-08 13:14:53 +0700

    chore: drawer

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from "./Card";
 export { default as Header } from "./Header";
 export { default as Pagination } from "./Pagination";
 export { default as SearchControl } from "./SearchControl";
+export { default as Drawer } from "./Drawer";
